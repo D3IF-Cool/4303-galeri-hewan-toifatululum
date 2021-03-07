afa498cbe9b49b0bf5e9acf735f55590c0d9090b
Toifatul_Ngulum <toifatululum222@gmail.com>
2021-03-07 11:25:24 +0700

    Menambahkan view binding

--- app/src/main/java/org/d3if0009/galerihewan/MainActivity.kt
+++ app/src/main/java/org/d3if0009/galerihewan/MainActivity.kt
@@ -3,27 +3,34 @@ package org.d3if0009.galerihewan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import org.d3if0009.galerihewan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Log.d("MainActivity", "Jumlah data: " + getData().size)
     }
 
     private fun getData(): List<Hewan> {
         return listOf(
-                Hewan("Angsa", "Cygnus olor"),
-                Hewan("Ayam", "Gallus gallus"),
-                Hewan("Bebek", "Cairina moschata"),
-                Hewan("Domba", "Ovis ammon"),
-                Hewan("Kalkun", "Meleagris gallopavo"),
-                Hewan("Kambing", "Capriconis sumatranesis"),
-                Hewan("Kelinci", "Oryctolagus cuniculus"),
-                Hewan("Kerbau", "Bubalus bubalis"),
-                Hewan("Kuda", "Equus caballus"),
-                Hewan("Sapi", "Bos taurus")
+            Hewan("Angsa", "Cygnus olor"),
+            Hewan("Ayam", "Gallus gallus"),
+            Hewan("Bebek", "Cairina moschata"),
+            Hewan("Domba", "Ovis ammon"),
+            Hewan("Kalkun", "Meleagris gallopavo"),
+            Hewan("Kambing", "Capriconis sumatranesis"),
+            Hewan("Kelinci", "Oryctolagus cuniculus"),
+            Hewan("Kerbau", "Bubalus bubalis"),
+            Hewan("Kuda", "Equus caballus"),
+            Hewan("Sapi", "Bos taurus")
         )
     }
 }
