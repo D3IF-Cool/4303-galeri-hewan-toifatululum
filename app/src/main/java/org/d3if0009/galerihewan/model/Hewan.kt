c1f2b4ea4cc10ca353baa418222252a084feca35
Toifatul_Ngulum <toifatululum222@gmail.com>
2021-05-03 22:11:26 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if0009/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if0009/galerihewan/model/Hewan.kt
@@ -1,8 +1,9 @@
 package org.d3if0009.galerihewan.model
 
+import org.d3if0009.galerihewan.R
+
 data class Hewan(
     val nama: String,
     val namaLatin: String,
-    val jenis: String,
-    val imageResId: Int
+    val imageResId: Int = R.drawable.angsa
 )
--- app/src/main/java/org/d3if0009/galerihewan/ui/main/MainViewModel.kt
+++ app/src/main/java/org/d3if0009/galerihewan/ui/main/MainViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import org.d3if0009.galerihewan.R
 import org.d3if0009.galerihewan.model.Hewan
 import org.d3if0009.galerihewan.network.HewanApi
 import java.lang.Exception
@@ -16,35 +15,18 @@ class MainViewModel : ViewModel() {
     private val data = MutableLiveData<List<Hewan>>()
 
     init {
-        data.value = initData()
         retrieveData()
     }
 
-    private fun retrieveData(){
+    private fun retrieveData() {
         viewModelScope.launch {
             try {
-                val result = HewanApi.service.getHewan()
-                Log.d("MainViewModel", "Success: $result")
+                data.value = HewanApi.service.getHewan()
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
             }
         }
     }
 
-    private fun initData(): List<Hewan> {
-        return listOf(
-            Hewan("Angsa", "Cygnus olor", "Unggas", R.drawable.angsa),
-            Hewan("Ayam", "Gallus gallus", "Unggas", R.drawable.ayam),
-            Hewan("Bebek", "Cairina moschata", "Unggas", R.drawable.bebek),
-            Hewan("Domba", "Ovis ammon", "Mamalia", R.drawable.domba),
-            Hewan("Kalkun", "Meleagris gallopavo", "Unggas", R.drawable.kalkun),
-            Hewan("Kambing", "Capriconis sumatranesis", "Mamalia", R.drawable.kambing),
-            Hewan("Kelinci", "Oryctolagus cuniculus", "Mamalia", R.drawable.kelinci),
-            Hewan("Kerbau", "Bubalus bubalis", "Mamalia", R.drawable.kerbau),
-            Hewan("Kuda", "Equus caballus", "Mamalia", R.drawable.kuda),
-            Hewan("Sapi", "Bos taurus", "Mamalia", R.drawable.sapi)
-        )
-    }
-
-    fun getData() : LiveData<List<Hewan>> = data
+    fun getData(): LiveData<List<Hewan>> = data
 }
