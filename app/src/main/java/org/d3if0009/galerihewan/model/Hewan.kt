7109017967255855ad7b6ab0a445ba598c3bd80f
Toifatul_Ngulum <toifatululum222@gmail.com>
2021-05-03 22:27:16 +0700

    Menampilkan data dari internet

--- app/src/main/java/org/d3if0009/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if0009/galerihewan/model/Hewan.kt
@@ -1,9 +1,7 @@
 package org.d3if0009.galerihewan.model
 
-import org.d3if0009.galerihewan.R
-
 data class Hewan(
     val nama: String,
     val namaLatin: String,
-    val imageResId: Int = R.drawable.angsa
+    val imageId: String
 )
